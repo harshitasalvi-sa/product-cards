4b4ba774ad9c057e84aed05f8915ba4e1c2a2f55
harshitasalvi-sa <harshita.salvi@startappss.com>
2026-01-06 17:05:26 +0530

    Built product cards using react

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import "./ProductCard.css";
+
+const ProductCard = ({ name, description, price, image, inStock }) => {
+  return (
+    <div className="product-card">
+      <img src={image} alt={name} />
+
+      <div className="product-info">
+        <h3>{name}</h3>
+        <p className="desc">{description}</p>
+        <p className="price">₹{price}</p>
+
+        <button
+          className={`buy-btn ${!inStock ? "disabled" : ""}`}
+          disabled={!inStock}
+        >
+          {inStock ? "Buy Now" : "Out of Stock"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
